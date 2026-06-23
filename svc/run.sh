41809fb9684fbae361ad3037bc6efd9bc7ceb625
Chrissy <chris@dylt.dev>
2026-06-23 04:03:32 +0000

    tactical service files moved to svc

---
+++ svc/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+SVC_ROOT=/opt/svc/bettywhitelist
+UDS_PATH="$SVC_ROOT/bettywhitelist.sock"
+LOGS_FOLDER="$SVC_ROOT/log"
+mkdir -p "$LOGS_FOLDER"
+exec "$SVC_ROOT/venv/bin/gunicorn" \
+  --bind "unix:$UDS_PATH" \
+  --access-logfile "$LOGS_FOLDER/access.log" \
+  --error-logfile "$LOGS_FOLDER/errors.log" \
+  --workers 3 \
+  --umask 007 \
+  --chdir "$SVC_ROOT/content" \
+  splunge.app:app
